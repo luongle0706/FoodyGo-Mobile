253697928b24a4d1054d7c33ae3f1da93cebbd06
AnhNT <anhntse183225@fpt.edu.vn>
2025-01-26 17:08:44 +0700

    app_route_config.dart

--- lib/projects/routes/app_route_config.dart
+++ lib/projects/routes/app_route_config.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:foodygo/pages/home.dart';
 import 'package:foodygo/pages/login.dart';
 import 'package:foodygo/pages/profile.dart';
@@ -6,43 +7,59 @@ import 'package:foodygo/pages/register.dart';
 import 'package:go_router/go_router.dart';
 
 class MyAppRouter {
-  static final MyAppRouter _instance =
-      MyAppRouter._internal(isAuthenticated: false);
+  static final MyAppRouter _instance = MyAppRouter._internal();
   factory MyAppRouter() => _instance;
-  MyAppRouter._internal({required this.isAuthenticated});
+  MyAppRouter._internal();
 
-  final bool isAuthenticated;
+  final storage = FlutterSecureStorage();
+
+  Future<bool> isAuthenticated() async {
+    String? token = await storage.read(key: 'token');
+    return token != null;
+  }
+
+  final List<String> publicRoutes = [
+    '/login',
+    '/register',
+  ];
 
   GoRouter router = GoRouter(
-    initialLocation: '/',
-    routes: [
-      GoRoute(
-          name: 'home',
-          path: '/',
-          pageBuilder: (context, state) {
-            return MaterialPage(child: HomePage());
-          }),
-      GoRoute(
-          name: 'login',
-          path: '/login',
-          pageBuilder: (context, state) {
-            return MaterialPage(child: LoginPage());
-          }),
-      GoRoute(
-          name: 'register',
-          path: '/register',
-          pageBuilder: (context, state) {
-            return MaterialPage(child: RegisterPage());
-          }),
-      GoRoute(
-          name: 'profile',
-          path: '/profile/:username',
-          pageBuilder: (context, state) {
-            return MaterialPage(
-                child: ProfilePage(
-              username: state.pathParameters['username'],
-            ));
-          })
-    ],
-  );
+      initialLocation: '/',
+      routes: [
+        GoRoute(
+            name: 'home',
+            path: '/',
+            pageBuilder: (context, state) {
+              return MaterialPage(child: HomePage());
+            }),
+        GoRoute(
+            name: 'login',
+            path: '/login',
+            pageBuilder: (context, state) {
+              return MaterialPage(child: LoginPage());
+            }),
+        GoRoute(
+            name: 'register',
+            path: '/register',
+            pageBuilder: (context, state) {
+              return MaterialPage(child: RegisterPage());
+            }),
+        GoRoute(
+            name: 'profile',
+            path: '/profile/:username',
+            pageBuilder: (context, state) {
+              return MaterialPage(
+                  child: ProfilePage(
+                username: state.pathParameters['username'],
+              ));
+            })
+      ],
+      redirect: (context, state) async {
+        final isAuthenticated = await _instance.isAuthenticated();
+        final isPublicRoute =
+            _instance.publicRoutes.contains(state.matchedLocation);
+        if (!isAuthenticated && !isPublicRoute) return '/login';
+        if (isAuthenticated && state.matchedLocation == '/login') return '/';
+        return null;
+      });
 }
